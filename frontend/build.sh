3f74188ab9d5be5ea34038e85b824f181feb12d8
Faisal Naveed <faisal.naveed.akbar@gmail.com>
2019-09-27 16:25:55 +0500

    build file updated

--- frontend/build.sh
+++ frontend/build.sh
@@ -11,7 +11,7 @@ elif [ "$1" = "production" ]
 then
   echo Building wallet with production Enviorment
 
- NODE_ENV=production VUE_APP_CHAIN=colors-test-01 VUE_APP_CLAIM_URL=https://proxy.testnet.color-platform.org:9000/claim yarn build
+ NODE_ENV=production VUE_APP_CHAIN=colors-test-01 VUE_APP_CLAIM_URL=https://proxy.testnet.color-platform.org:8010/claim yarn build
 else 
   echo Environment not provided, e.g staging
 fi
